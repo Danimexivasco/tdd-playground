f5c7c9c3c1f849c51ce4fe1ca48e55199df29283
Danimexivasco <dcano9493@gmail.com>
2025-01-12 12:12:54 +0100

    Create client layout to render our app

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import {createRoot} from "react-dom/client";
+import React from "react";
+import { Calculator } from "./components/Calculator";
+
+createRoot(document.getElementById("root")).render(<Calculator />);
