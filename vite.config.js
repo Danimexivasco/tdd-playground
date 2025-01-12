ea002339b321538d69c4903e45ef6222432e78d8
Danimexivasco <dcano9493@gmail.com>
2025-01-12 13:39:23 +0100

    Format stylistically vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test: { // Vitest config
+  test:    { // Vitest config
     environment: "happy-dom"
   }
 });
